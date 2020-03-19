06985f947577684ae947410e48eb1df54980c887
李保君 <lbj147123@163.com>
2020-03-19 17:26:22 +0800

    fix: 格式化

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'pages/login/login.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-
   final Color primaryColor = Color(0xff03da9d);
   final Color accentColor = Color(0xff333333);
 
@@ -15,39 +14,37 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: '企鹅工具链',
       theme: ThemeData(
-          primaryColor: primaryColor,
-          accentColor: accentColor,
-          scaffoldBackgroundColor: Colors.white,
-          appBarTheme: AppBarTheme(color: Colors.white),
-          fontFamily: 'Rubik',
-          textTheme: TextTheme(
-            headline:
-                TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
-            button: TextStyle(color: Colors.white),
-          ),
-          buttonTheme: ButtonThemeData(
-            buttonColor: primaryColor,
-            textTheme: ButtonTextTheme.normal,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            contentPadding: EdgeInsets.all(8),
-            filled: true,
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-              borderSide: BorderSide(style: BorderStyle.none),
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
+        primaryColor: primaryColor,
+        accentColor: accentColor,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(color: Colors.white),
+        fontFamily: 'Rubik',
+        textTheme: TextTheme(
+          headline:
+              TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
+          button: TextStyle(color: Colors.white),
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: primaryColor,
+          textTheme: ButtonTextTheme.normal,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
           ),
         ),
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: EdgeInsets.all(8),
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(style: BorderStyle.none),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
       home: Login(),
       // home: LifecycleAppPage(),
     );
   }
 }
-
-
