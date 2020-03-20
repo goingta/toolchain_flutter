4b94ed4a20c6a3ae02b6aece95cdf5a371e9fbe2
李保君 <lbj147123@163.com>
2020-03-20 18:08:43 +0800

    fix: 添加新页面，添加插件。

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:toolchain_flutter/model/user_model.dart';
 import 'package:toolchain_flutter/pages/tabPage/tabPage.dart';
 import 'pages/tabPage/tabPage.dart';
+import 'pages/tabPage/homePage.dart';
 import 'pages/login/login.dart';
 import 'model/user.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-
   final Color primaryColor = Color(0xff03da9d);
   final Color accentColor = Color(0xff333333);
 
@@ -17,43 +17,42 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: '企鹅工具链',
       theme: ThemeData(
-          primaryColor: primaryColor,
-          accentColor: accentColor,
-          scaffoldBackgroundColor: Colors.white,
-          appBarTheme: AppBarTheme(color: Colors.white),
-          fontFamily: 'Rubik',
-          textTheme: TextTheme(
-            headline:
-                TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
-            button: TextStyle(color: Colors.white),
-          ),
-          buttonTheme: ButtonThemeData(
-            buttonColor: primaryColor,
-            textTheme: ButtonTextTheme.normal,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            contentPadding: EdgeInsets.all(8),
-            filled: true,
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-              borderSide: BorderSide(style: BorderStyle.none),
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
+        primaryColor: primaryColor,
+        accentColor: accentColor,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(color: Colors.white),
+        fontFamily: 'Rubik',
+        textTheme: TextTheme(
+          headline:
+              TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
+          button: TextStyle(color: Colors.white),
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: primaryColor,
+          textTheme: ButtonTextTheme.normal,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
           ),
         ),
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: EdgeInsets.all(8),
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(style: BorderStyle.none),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
       home: FutureBuilder<UserModel>(
-          future: User.getCurrentUser(),
-          builder: (context, AsyncSnapshot<UserModel> user) {
-            return user.hasData
-                  ? TabPage()
-                  : LoginPage();
-          },
-        ),
+        future: User.getCurrentUser(),
+        builder: (context, AsyncSnapshot<UserModel> user) {
+          return user.hasData ? TabPage() : LoginPage();
+        },
+      ),
+      // home: HomePage(),
       routes: {
         LoginPage.id: (context) => LoginPage(),
         TabPage.id: (context) => TabPage()
@@ -62,5 +61,3 @@ class App extends StatelessWidget {
     );
   }
 }
-
-
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,20 +1,18 @@
 import 'dart:async';
 import '../model/user_model.dart';
+import './model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class User {
   static Future<String> signIn(String email, String password) async {
-
     return "";
   }
 
-  static Future<void> signOut() async {
-
-  }
+  static Future<void> signOut() async {}
 
   static Future<UserModel> getCurrentUser() async {
     // UserModel user = UserModel("goingta", "", "");
     UserModel user = null;
     return user;
   }
-
 }
