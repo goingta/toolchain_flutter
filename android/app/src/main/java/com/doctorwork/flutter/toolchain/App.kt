0eb344b05c225e0fb82ce07fca441e080a559145
jiahuan <abbottyu@xingren.com>
2020-09-01 09:55:17 +0800

    重新调整项目依赖，iOS以及Android 工程

--- android/app/src/main/java/com/doctorwork/flutter/toolchain/App.kt
+++
@@ -1,9 +0,0 @@
-package com.doctorwork.flutter.toolchain
-
-import io.flutter.app.FlutterApplication
-
-class App : FlutterApplication() {
-    companion object {
-        const val APP_ID = "wx36017dd6944033f4"
-    }
-}
--- android/app/src/main/java/com/doctorwork/flutter/toolchain/MainActivity.kt
+++ android/app/src/main/java/com/doctorwork/flutter/toolchain/MainActivity.kt
@@ -2,13 +2,11 @@ package com.doctorwork.flutter.toolchain
 
 import android.os.Bundle
 import com.doctorwork.flutter.toolchain.plugins.WeChatPlugin
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-        WeChatPlugin.registerWith(flutterView)
+        flutterEngine?.plugins?.add(WeChatPlugin())
     }
 }
